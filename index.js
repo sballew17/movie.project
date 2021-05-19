70e4caa118a6d45304472d8869fd814f37538638
sballew17 <samuel.ballew17@gmail.com>
2021-05-19 19:08:33 -0400

    Update index.js

--- index.js
+++ index.js
@@ -160,6 +160,7 @@ app.delete('/user/:Username', passport.authenticate('jwt', {session: false}), (r
   });
 
 //listen for requests
-app.listen(8080) => {
-console.log('Your app is listening on port 8080.');
+const port = process.env.PORT || 8080;
+app.listen(port, '0.0.0.0',() => {
+ console.log('Listening on Port ' + port);
 });
