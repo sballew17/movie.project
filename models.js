f00391d78175c9437be5d8508a695dd7b522c422
sballew17 <samuel.ballew17@gmail.com>
2021-06-12 21:13:54 -0400

    Added Validation
    Adding validation for passwords

--- models.js
+++ models.js
@@ -14,6 +14,8 @@ let movieSchema = mongoose.Schema({
   }
 });
 
+const bcrypt = require('bcrypt');
+
 let userSchema = mongoose.Schema({
   Username: {type: String, required: true},
   Password: {type: String, required: true},
@@ -21,6 +23,14 @@ let userSchema = mongoose.Schema({
   Birthday: Date,
   FavoriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movie'}]
 });
+
+userSchema.statics.hashPassword = (password) => {
+  return bcrypt.hashSync(password, 10);
+};
+
+userSchema.methods.validatePassword = function(password) {
+  return bcrypt.compareSync(password, this.Password);
+};
 
 let Movie = mongoose.model('Movie', movieSchema);
 let User = mongoose.model('User', userSchema);
